7e9d650837f2b5195f4101564367888b944ba475
xrinise <58216065+xrinise@users.noreply.github.com>
2020-10-02 22:22:00 +0800

    Add files via upload

---
+++ index.php
@@ -0,0 +1,86 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title> Web Animation Exam</title>
+<link href="style.css"rel="stylesheet" type="text/css">
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+
+</head>
+<body>
+      <header>
+
+              <div class="lang">
+                <select name="Language" id="Language">
+                  <option value="">--Select Language--</option>
+                  <option value="English">English</option>
+                  <option value="Chinese">Chinese</option>
+                  <option value="Russian">Russian</option>
+                  <option value="Japanese">Japanese</option>
+                </select>
+              </div>
+
+              <div class="plat">
+                <select name="Platform" id="Platform">
+                  <option value="">--Select Platform--</option>
+                  <option value="Steam">Steam</option>
+                  <option value="Xbox">Xbox</option>
+                  <option value="PlayStation">PlayStation</option>
+                </select>
+              </div>
+
+              <div class="logo">
+                  <div id="tower1"></div>
+                  <div id="tower4"></div>
+                  <div id="tower2"></div>
+                  <div id="tower5"></div>
+                  <div id="tower3"></div>
+                  <div id="tower6"></div>
+              </div>
+
+              <div id=container>
+                75% Discount on
+              <div id=flip>
+                <div><div>Music Pack</div></div>
+                <div><div>Cosmetics</div></div>
+                <div><div>Iceborne</div></div>
+              </div>
+                Buy now!
+              </div>
+
+              <p> Monster Hunter Store </p>
+
+            </header>
+
+              <content>
+                    <div class="text1">
+                    <h1> Monster Hunter World </h1>
+                    </div>
+
+                    <div class="text2">
+                    <h2> About this game </h2><h3><br> an action role-playing game developed and published by Capcom,
+                      <br>and the fifth mainline installment in the Monster Hunter series.</h3>
+                    </div>
+
+                    <div class="text3">
+                    <h2> Join the hunt </h2><h3><br> It's time to embark on the ultimate hunting experience in open-world action RPG Monster Hunter: World.
+                      <br> With this Steam key, you'll create your own customizable Hunter character and a Palico (cat) companion, and set out on a big hunt to locate dangerous monsters across unique areas of the New World.</h3>
+                    </div>
+
+                    <div class="blink"><span>Buy Now!</span></div>
+
+            </content>
+
+            <footer>
+
+        <h6>Connect With Us</h6>
+
+                <div class="style">
+                  <i class="fa fa-facebook-square" aria-hidden="true"></i>
+                  <i class="fa fa-twitter" aria-hidden="true"></i>
+                  <i class="fa fa-instagram" aria-hidden="true"></i>
+                </div>
+
+            </footer>
+
+</body>
+</html>
